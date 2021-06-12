8777987d26a223c8bcb78fc147a9e920bf7059fe
Gohar Attiq <goharattiq@hotmail.com>
2021-06-12 10:43:46 +0500

    deployment issue resolved

--- client/src/redux/alert/alertAction.js
+++ client/src/redux/alert/alertAction.js
@@ -1,17 +1,17 @@
-import uuid from 'uuid';
+import { v4 as uuidv4 } from 'uuid';
 
-export const setAlert =(msg,alertType)=>{
-    const id = uuid.v4();
+export const setAlert = (msg, alertType) => {
+    const id = uuidv4();
     return {
-        type:'SET_ALERT',
-        payload:{msg,alertType,id}
+        type: 'SET_ALERT',
+        payload: { msg, alertType, id }
     }
-    
+
 }
 
-export const removeAlert =(id)=>{
+export const removeAlert = (id) => {
     return {
-        type:'REMOVE_ALERT',
+        type: 'REMOVE_ALERT',
         payload: id
     }
 }
