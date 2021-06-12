11885f9691b039612e099a7bae9dfa71de533b37
Gohar Attiq <goharattiq@hotmail.com>
2021-06-12 15:01:44 +0500

    env vars update

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,19 @@
 const jwt = require('jsonwebtoken'),
-      config=require('config');
+    config = require('config');
 
-module.exports = function (req,res,next){
+module.exports = function(req, res, next) {
     const token = req.header('x-auth-token');
-    
-    if(!token){
-        return res.status(401).json({msg:"No tokken, authorization denied"});
+
+    if (!token) {
+        return res.status(401).json({ msg: "No tokken, authorization denied" });
     }
 
     try {
-        const decoded = jwt.verify(token,config.get('jwtsecret'));
-        req.user=decoded;
-        
+        const decoded = jwt.verify(token, process.env.jwtsecret || config.get('jwtsecret'));
+        req.user = decoded;
+
         next();
     } catch (error) {
-        res.status(401).json({msg:"Token is invalid"})
+        res.status(401).json({ msg: "Token is invalid" })
     }
 }
-
-//mongodb://localhost:27017/learn
